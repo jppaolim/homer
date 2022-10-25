f1f8ced1d0eea195bc8f32afa1cc711ee3cd1c29
Jean-Paul Paoli <contact@paolim.fr>
2022-10-25 02:11:38 +0200

    server update

--- pages/api/serverSideCall.js
+++ pages/api/serverSideCall.js
@@ -1,8 +1,5 @@
 export default async function serverSideCall(req,res ) {
     
-
-    //console.log(JSON.stringify(req.body));
-
     const response =  await fetch(
 		"https://api-inference.huggingface.co/models/jppaolim/homerGPT2",
 		{
@@ -11,24 +8,13 @@ export default async function serverSideCall(req,res ) {
 			body: JSON.stringify(req.body),
 		}
 	);  
-  //).then(response => response.json())
-  //.then(data => console.log(data));
-  
-  //if (response.ok) {
-
-   //  console.log(response)
-   // console.log(JSON.stringify(data[0]['generated_text']))
 
    const result = await response.json();
    console.log("Result : ");
    console.log(result);
 
-//  }
-        
-
    res.status(200).json({
         data: result  ,
     });
-
   
 }
