a3d747a99fe8bfc473296845e66cf99dfa8e58e5
Jean-Paul Paoli <contact@paolim.fr>
2022-10-28 02:46:42 +0200

    manage to retry

--- pages/api/serverSideCall.js
+++ pages/api/serverSideCall.js
@@ -1,7 +1,7 @@
 export default async function serverSideCall(req, res) {
   const response = await fetch(
    "https://api-inference.huggingface.co/models/jppaolim/homerGPT2",
-//  "https://api-inference.huggingface.co/models/jppaolim/v60_Large_2E",
+// "https://api-inference.huggingface.co/models/jppaolim/v60_Large_2E",
     
     {
       headers: { Authorization: "Bearer " + process.env.KEY },
